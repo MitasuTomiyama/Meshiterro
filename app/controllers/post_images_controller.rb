d36d445e4f5bed665a3fbb47acdca77c248854a7
MitasuTomiyama <m.tomiyama52@gmail.com>
2020-02-13 20:42:28 +0900

    [Add]画像投稿機能

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -6,7 +6,7 @@ class PostImagesController < ApplicationController
 
 	def create
 		@post_image = PostImage.new(post_image_params)
-		@post_image.user_id = current_user.user_id
+		@post_image.user_id = current_user.id
 		@post_image.save
 		redirect_to post_images_path
 	end
@@ -19,6 +19,6 @@ class PostImagesController < ApplicationController
 
 	private
 	def post_image_params
-		params.require(:post_image).permit(:shop_name, :image, :caption)
+        params.require(:post_image).permit(:shop_name, :image, :caption)
 	end
 end
