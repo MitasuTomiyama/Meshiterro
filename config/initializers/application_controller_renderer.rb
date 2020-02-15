988392a3a5ae007eaa26ab1740aaead4152e5a2e
MitasuTomiyama <m.tomiyama52@gmail.com>
2020-02-15 14:52:53 +0900

    [Add]コメント機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f9335a38647391e141d6c97cdce16537131942f9fd1a0156bd6cbdfebdc9a8fb3d8d9db371c5bbd8d525564212c70cabf15a33afeb213648bf1a86075a11d09c'
